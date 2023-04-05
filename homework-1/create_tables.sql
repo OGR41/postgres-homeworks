7a70b892d410cebc908228128bf72c0ef991405f
chepl_89zffkl <alexorgchep@ya.ru>
2023-04-05 23:09:55 +1200

    HW 17.1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	first_name text PRIMARY KEY,
+	last_name text,
+	title text,
+	birth_date text,
+	notes text
+);
+
+
+CREATE TABLE customers
+(
+	customer_id text PRIMARY KEY,
+	company_name text,
+	contact_name text
+);
+
+CREATE TABLE orders
+(
+	order_id text PRIMARY KEY,
+	customer_id text,
+	employee_id text,
+	order_date text,
+	ship_city text
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,26 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password='Xperia99') as conn:
+
+    with conn.cursor() as cur:
+        sql = "COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','"
+        e = open('north_data/employees_data.csv', 'r', encoding='utf8')
+        cur.execute("truncate " + 'employees' + ";")
+        cur.copy_expert(sql=sql % 'employees', file=e)
+
+        c = open('north_data/customers_data.csv', 'r', encoding='utf8')
+        cur.copy_from(c, 'customers', sep=',')
+
+        o = open('north_data/orders_data.csv', 'r', encoding='utf8')
+        cur.copy_from(o, 'orders', sep=',')
+
+        conn.commit()
+
+cur.close()
+conn.close()
+
+e.close()
+c.close()
+o.close()
