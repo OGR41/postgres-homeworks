84272fc706eff88f7c19aea4cb2125ff0a10bfeb
chepl_89zffkl <alexorgchep@ya.ru>
2023-04-10 21:47:02 +1200

    HW 17.1

--- homework-1/main.py
+++ homework-1/main.py
@@ -11,13 +11,11 @@ with psycopg2.connect(host='localhost', database='north', user='postgres', passw
         cur.execute("truncate " + 'employees' + ";")
         cur.copy_expert(sql=sql % 'employees', file=e)
 
-        sql = "COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','"
         c = open('north_data/customers_data.csv', 'r', encoding='utf8')
         next(c)
         cur.execute("truncate " + 'customers' + ";")
         cur.copy_expert(sql=sql % 'customers', file=c)
 
-        sql = "COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','"
         o = open('north_data/orders_data.csv', 'r', encoding='utf8')
         next(o)
         cur.execute("truncate " + 'orders' + ";")
